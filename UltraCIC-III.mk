e7eaca579e09cc8167617d0506b1ae152d6e0280
ManCloud <ch_ruecker@t-online.de>
2020-07-28 13:57:06 +0200

    Update UltraCIC-III.mk
    corrected fuses

--- UltraCIC-III.mk
+++ UltraCIC-III.mk
@@ -28,32 +28,32 @@ $(TARGET).hex: $(PROJECT).asm
 #
 # FUSE low byte
 #
-# 7: SPIEN    -> 0
-# 6: EESAVE   -> 1
-# 5: WDTON    -> 1
-# 4: CKDIV8   -> 1
-# 3: SUT1     -> 1
-# 2: SUT0     -> 0
-# 1: CKSEL1   -> 1
-# 0: CKSEL0   -> 1
+# 7: CKDIV8   -> 1
+# 6: CKOUT    -> 1
+# 5: SUT1     -> 0
+# 4: SUT0     -> 0
+# 3: CKSEL3   -> 0
+# 2: CKSEL2   -> 0
+# 1: CKSEL1   -> 0
+# 0: CKSEL0   -> 0
 #
 # FUSE high byte
 #
-# 7: RESERVED  -> 1
-# 6: RESERVED  -> 1
-# 5: RESERVED  -> 1
-# 4: SELFPRGEN -> 1
-# 3: DWEN      -> 1
-# 2: BODLEVEL1 -> 1
-# 1: BODLEVEL0 -> 1
-# 0: RSTDISBL  -> 0
+# 7: RSTDISBL  -> 1
+# 6: DWEN      -> 1
+# 5: SPIEN     -> 0
+# 4: WDTON     -> 1
+# 3: EESAVE    -> 1
+# 2: BODLEVEL2 -> 1
+# 1: BODLEVEL1 -> 1
+# 0: BODLEVEL0 -> 1
 #
 
 fuse:
-	$(AVRDUDE) -p $(AVRDUDE_CPU) -P usb -c $(PROGRAMMER) -Uhfuse:w:0xfe:m -Ulfuse:w:0x7b:m -B 64.0 -F
+	$(AVRDUDE) -p $(AVRDUDE_CPU) -P usb -c $(PROGRAMMER) -Uhfuse:w:0xdf:m -Ulfuse:w:0xc0:m
 
 flash: $(HEXFILE)
-	$(AVRDUDE) -p $(AVRDUDE_CPU) -P usb -c $(PROGRAMMER) -Uflash:w:$(PROJECT).hex -B 64.0 -F
+	$(AVRDUDE) -p $(AVRDUDE_CPU) -P usb -c $(PROGRAMMER) -Uflash:w:$(PROJECT).hex
 
 reset:
-	$(AVRDUDE) -p $(AVRDUDE_CPU) -P usb -c $(PROGRAMMER) -B 64.0 -F
+	$(AVRDUDE) -p $(AVRDUDE_CPU) -P usb -c $(PROGRAMMER)
